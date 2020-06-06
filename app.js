71647895275939817545cb12a63c516427d8ca2e
alex8430 <pv24aug2001@gmail.com>
2020-06-06 20:31:26 +0530

    Intial Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB",{ useNewUrlParser: true , useUnifiedTopology: true });
+
+const  articleSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article",articleSchema);
+
+app.get("/articles", function(req,res){
+  Article.find({},function(err,foundArticles){
+    if(err){
+      res.send(err);
+    }else{
+      res.send(foundArticles);
+    }
+  });
+});
+
+app.post("/articles",function(req,res){
+  const newArticle = new Article({
+    title:req.body.title,
+    content:req.body.content
+  });
+  newArticle.save(function(err){
+    if(err){
+      res.send(err);
+    }else{
+      res.send("Succesfully added new article");
+    }
+  });
+});
+
+app.listen(3000, function(){
+  console.log("server is start on port 3000");
+});
